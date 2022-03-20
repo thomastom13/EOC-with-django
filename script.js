33462dbd9d1cf4493471a0ff79606e4ecb97a0c1
thomastom13 <100709978+thomastom13@users.noreply.github.com>
2022-03-20 22:46:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// // var img =new Image();
+// // img.src="./props/";
+// // // document.getElementById('img')
+// //  containerElement.appendChild(img)
+
+
+// const props=document.images.item(0).src;
+// let text="";
+// for(let i =0;i< props.length;i++){
+//     text+= props[i].src+"<br>";
+// }
